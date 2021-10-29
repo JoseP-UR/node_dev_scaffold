8dba9e79c888ed6f9342d652b15bca38126c297e
JosePUR <urivesrosa@gmail.com>
2021-10-29 12:40:24 -0300

    fixed mongo-express

--- nodeserver/src/server.ts
+++ nodeserver/src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 const APP_PORT = process.env.APP_PORT || 3000;
 
 app.get('/api/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello Worlasdasdasd!');
 });
 
 app.listen(APP_PORT, () => {
