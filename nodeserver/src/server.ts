8ada76b400a93d8aff6f2e115fec9a454c025d9d
Jose Paulo Urives <jose.rosa@bairesdev.com>
2022-05-18 01:07:52 -0300

    changed from vue-clie and vue2 to vite and vue3

--- nodeserver/src/server.ts
+++ nodeserver/src/server.ts
@@ -4,8 +4,8 @@ const app = express();
 
 const APP_PORT = process.env.APP_PORT || 3000;
 
-app.get('/api/', (req, res) => {
-    res.send('Hello Worlasdasdasd!');
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 app.listen(APP_PORT, () => {
